caf95932831f3625d5770ac07b19011187d71a23
Shaila Akter <shailaakterss@gmail.com>
2024-07-13 07:54:29 +0600

    404 added

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,20 @@
+import ContainerBox from "../components/containers/ContainerBox"
+import error from "../assets/images/error.png"
+import YellowButton from "../components/buttons/YellowButton"
+
 
 const PageNotFound = () => 
 {
   return (
-    <div>PageNotFound ok?</div>
+    <ContainerBox title="404 | Page Not Found!">
+      <div className="h-screen flex justify-center items-center text-center">
+        <div>
+          <img className="lg:max-w-3xl w-full h-auto transition-transform duration-300 transform-gpu hover:scale-110 " src={error} alt="notFound" />
+        
+          <YellowButton to="/">Go Back to Home</YellowButton>
+        </div>
+      </div>
+    </ContainerBox>
   )
 }
 
